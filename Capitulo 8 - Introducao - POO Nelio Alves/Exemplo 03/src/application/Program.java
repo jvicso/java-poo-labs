deca244bdf7ca331b6b0f126160d1745f61c3ecb
Joao Victor <jvss.smc@gmail.com>
2023-05-21 19:00:04 -0300

    exemplo 03 v3

--- Capitulo 8 - Introducao - POO Nelio Alves/Exemplo 03/src/application/Program.java
+++ Capitulo 8 - Introducao - POO Nelio Alves/Exemplo 03/src/application/Program.java
@@ -10,17 +10,13 @@ public class Program {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        Calculator calc = new Calculator();
 
         System.out.print("Enter radius: ");
         double radius = sc.nextDouble();
 
-        double c = calc.circumference(radius);
-        double v = calc.volume(radius);
-
-        System.out.printf("Circumference: %.2f%n", c);
-        System.out.printf("Volume: %.2f%n", v);
-        System.out.printf("PI value: %.2f%n", calc.PI);
+        System.out.printf("Circumference: %.2f%n", Calculator.circumference(radius));
+        System.out.printf("Volume: %.2f%n", Calculator.volume(radius));
+        System.out.printf("PI value: %.2f%n", Calculator.PI);
 
         sc.close();
     }
